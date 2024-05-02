25f15ad4ec745e9f6a5996a3cd4c7e579e447eaa
Nguyen Le Anh <75296157+nlanh@users.noreply.github.com>
2024-05-02 14:24:56 +0700

    Add files via upload

--- pot_read.for
+++ pot_read.for
@@ -1,15 +1,16 @@
       program pot
       
       implicit real*8(a-h,o-z)
-      dimension vn(1000)
+      dimension rn(1000),vn(1000)
       
       open(unit = 1, file = 'potin.dat')
       open(unit = 2, file = 'potout.dat')      
       read(1,*)h,e0,rmax
-      n = int((rmax-e0)/h+1e-8)              
-
+      n = int(rmax/h+1e-8)              
+      print*, h,e0,rmax
        do i = 1, n
-        write(1,*) r, vn(i)
+        read(1,*) rn(i), vn(i)
+        print*, rn(i), vn(i)
        end do
        write(2,'(10E14.7)')(vn(i),i=1,n)
 
